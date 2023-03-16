314a8d4973e2565289755aae8492ac45847f6711
Hand <Hand@2107475922@qq.com>
2023-03-16 08:44:30 +0800

    thrid commit

--- src/main/java/org/example/MyGit.java
+++ src/main/java/org/example/MyGit.java
@@ -4,5 +4,6 @@ public class MyGit {
     public static void main(String[] args) {
         System.out.println("fjdsaik");
         System.out.println("chuzi");
+        System.out.println("thrid");
     }
 }
